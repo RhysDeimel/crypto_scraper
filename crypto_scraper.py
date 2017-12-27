06e886cd2f382b9c6f1bfa24615fe6f0826fcf80
RhysDeimel <rhys.deimel@cammy.com>
2017-12-27 16:54:41 +1100

    added working API calls and main function

--- crypto_scraper.py
+++ crypto_scraper.py
@@ -18,42 +18,49 @@
 
 import requests
 import json
-
-#############
-# API calls
-#############
+import time
+import pprint
+import secrets
 
 # TODO: Change mocks below to actual calls that work with testing
 #   - make a secrets file to store API keys
 
 
-#############
-# Processing
-#############
-
 class Crypto_Assets():
     """Stores a tally of all obtained crypto and value"""
 
-    def __init__(self):
+    def __init__(self, testing=False):
+        self.time = time.time()
         self.coins = {
             item['coin']: item['confirmed'] for item in
-            self.get_raw_miningpoolhub_data()['getuserallbalances']['data']
+            self._get_raw_miningpoolhub_data(testing)['getuserallbalances']['data']
         }
         self.price = {
             item['id']: float(item['price_aud']) for item in
-            self.get_raw_coinmarketcap_data()
+            self._get_raw_coinmarketcap_data(testing)
             if item['id'] in self.coins.keys()
         }
 
-    def get_raw_miningpoolhub_data(self):
+    def _get_raw_miningpoolhub_data(self, testing):
         """Stub that reads in static file. Replace with API call"""
-        stub = open('tests/miningpoolhub_stub.json')
-        return json.load(stub)
+        if testing:
+            stub = open('tests/miningpoolhub_stub.json')
+            return json.load(stub)
+        else:
+            url = 'https://miningpoolhub.com/index.php?page=api&action=getuserallbalances&api_key={}'.format(secrets.api_key)
+            r = requests.get(url)
+            r.raise_for_status()
+            return json.loads(r.text)
 
-    def get_raw_coinmarketcap_data(self):
+    def _get_raw_coinmarketcap_data(self, testing):
         """Stub that reads in static file. Replace with API call"""
-        stub = open('tests/coinmarketcap_stub.json')
-        return json.load(stub)
+        if testing:
+            stub = open('tests/coinmarketcap_stub.json')
+            return json.load(stub)
+        else:
+            r = requests.get('https://api.coinmarketcap.com/v1/ticker/?convert=AUD')
+            r.raise_for_status()
+            return json.loads(r.text)
 
     def get_value(self, coin_dict, price_dict):
         """Given a dictionary of coins, will return the AUD value of held coins"""
@@ -77,8 +84,18 @@ class Crypto_Assets():
         coin_values = [v for v in coin_value_dict.values() if type(v) == float]
         return sum(coin_values)
 
-
 # TODO: store instance of object for later use
 #   - comparing how much crypto earnt since last run
 #   - comparing value of earnt crypto since last run
-#   - add timestamp for future data analysis?
+
+
+if __name__ == "__main__":
+    assets = Crypto_Assets()
+    coin_values = assets.get_value(assets.coins, assets.price)
+    total_value = assets.tally_crypto(coin_values)
+
+    print("\nCoins held:")
+    pprint.pprint(assets.coins,)
+    print("\nValue of individual coins:")
+    pprint.pprint(coin_values)
+    print("\nTotal: {}\n".format(total_value))
