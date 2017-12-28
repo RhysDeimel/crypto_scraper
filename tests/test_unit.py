5afa4136f5158be1f59771e6ffd68c7ee7816588
RhysDeimel <rhys.deimel@cammy.com>
2017-12-28 16:14:51 +1100

    updated to use new stub values

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -7,10 +7,10 @@ import crypto_scraper as cs
 def test_dict_of_coins_and_values_is_extracted_from_miningpoolhub():
     expected = {"bitcoin": 0.00107096,
                 "vertcoin": 0.50628363,
-                "feathercoin": 1.40910082,
+                "notarealcoin": 1.40910082,
                 "ethereum-classic": 0.00283998,
                 "zcash": 0.00027895,
-                "zclassic": 0.02357081,
+                "anothertestcoin": 0.02357081,
                 "zcoin": 0.15349718,
                 "monacoin": 0.83877923,
                 "zencash": 0.00088039,
@@ -22,14 +22,14 @@ def test_dict_of_coins_and_values_is_extracted_from_miningpoolhub():
 
 def test_prices_extracted_from_coinmarketcap_when_held():
     # omits coins not on the exchange
-    expected = {"bitcoin": 22684.7153676,
-                "vertcoin": 10.0374294907,
-                "ethereum-classic": 39.4525977846,
-                "zcash": 607.009606632,
-                "zcoin": 69.0804662691,
-                "monacoin": 18.5676934014,
-                "zencash": 43.3666257024,
-                "bitcoin-gold": 387.915625383}
+    expected = {"bitcoin": 19116.2005688,
+                "vertcoin": 8.646246496,
+                "ethereum-classic": 39.1196373715,
+                "zcash": 671.040801865,
+                "zcoin": 153.940829827,
+                "monacoin": 14.0370274167,
+                "zencash": 53.8790973101,
+                "bitcoin-gold": 378.887596475}
 
     assets = cs.Crypto_Assets(testing=True)
     assert assets.price == expected
@@ -37,7 +37,7 @@ def test_prices_extracted_from_coinmarketcap_when_held():
 
 def test_get_value_returns_single_correct_price():
     given = {"bitcoin": 0.00107096}
-    expected = {"bitcoin": 24.29}
+    expected = {"bitcoin": 20.47}
     assets = cs.Crypto_Assets(testing=True)
 
     assert assets.get_value(given, assets.price) == expected
@@ -53,22 +53,22 @@ def test_get_value_returns_multiple_correct_price():
              "zencash": 0.00088039,
              "bitcoin-gold": 0.00197191}
 
-    expected = {"bitcoin": 24.29,
-                "vertcoin": 5.08,
+    expected = {"bitcoin": 20.47,
+                "vertcoin": 4.38,
                 "ethereum-classic": 0.11,
-                "zcash": 0.17,
-                "zcoin": 10.60,
-                "monacoin": 15.57,
-                "zencash": 0.04,
-                "bitcoin-gold": 0.76}
+                "zcash": 0.19,
+                "zcoin": 23.63,
+                "monacoin": 11.77,
+                "zencash": 0.05,
+                "bitcoin-gold": 0.75}
     assets = cs.Crypto_Assets(testing=True)
 
     assert assets.get_value(given, assets.price) == expected
 
 
 def test_get_value_returns_str_when_no_price():
-    given = {"feathercoin": 1.40910082}
-    expected = {"feathercoin": "No price data"}
+    given = {"notarealcoin": 1.40910082}
+    expected = {"notarealcoin": "No price data"}
     assets = cs.Crypto_Assets(testing=True)
 
     assert assets.get_value(given, assets.price) == expected
@@ -76,9 +76,9 @@ def test_get_value_returns_str_when_no_price():
 
 def test_get_value_returns_str_for_no_price_when_mixed():
     given = {"bitcoin": 0.00107096,
-             "feathercoin": 1.40910082}
-    expected = {"bitcoin": 24.29,
-                "feathercoin": "No price data"}
+             "notarealcoin": 1.40910082}
+    expected = {"bitcoin": 20.47,
+                "notarealcoin": "No price data"}
     assets = cs.Crypto_Assets(testing=True)
 
     assert assets.get_value(given, assets.price) == expected
@@ -102,10 +102,10 @@ def test_tally_crypto_sums_held_crypto():
 def test_tally_crypto_ingnores_no_data_values():
     given = {"bitcoin": 24.29,
              "vertcoin": 5.08,
-             "feathercoin": "No price data",
+             "notarealcoin": "No price data",
              "ethereum-classic": 0.11,
              "zcash": 0.17,
-             "zclassic": "No price data",
+             "anothertestcoin": "No price data",
              "zcoin": 10.60,
              "monacoin": 15.57,
              "zencash": 0.04,
