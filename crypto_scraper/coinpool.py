4e3c0a938be6a8aac73ff548592f722874fd5499
RhysDeimel <rhys.deimel@nirovision.com>
2018-06-24 20:27:47 +1000

    pushing for desktop work

--- crypto_scraper/coinpool.py
+++ crypto_scraper/coinpool.py
@@ -17,12 +17,18 @@ class MiningPoolHub(Pool):
     def __init__(self):
         super().__init__()
         self.name = "MiningPoolHub"
+        self.coins = ""
 
     def fetch_raw_data(self, url=URL):
-        """Calls the api and returns a json blob of coins + amounts"""
+        """Calls the api and returns a raw api json response cointaining coins +
+         amounts"""
         r = requests.get(url)
         r.raise_for_status()
         return r.json()
+
+    def filter_only_coins(self, raw_api_json):
+        # return raw_api_json
+        pass
 
     def confirmed_coins(self):
         pass
--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,5 +1,8 @@
+import pytest
 import time
+import json
 from crypto_scraper import coinpool
+import os
 
 
 class TestPool:
@@ -15,4 +18,21 @@ class TestMiningPoolHub:
 
         assert first.date != second.date
 
+    def test_filter_only_coins_returns_list_of_coindicts(small_response):
+        # print(os.getcwd())
+        print(type(small_response))
+        snapshot = coinpool.MiningPoolHub()
+        only_coins = snapshot.filter_only_coins(small_response)
+        assert len(only_coins) == 3
 
+@pytest.fixture
+def small_response():
+    with open("tests/data/miningpoolhub_small.json", 'r') as f:
+        asdf = json.load(f)
+    return asdf
+
+@pytest.fixture
+def large_response():
+    with open("data/miningpoolhub_large.json", 'r') as f:
+        response = json.load(f)
+    return response
